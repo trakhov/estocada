54bd7c5f81ce52083766bc209a9ae7ef7bdbe36d
trakhov <igor.trakhov@gmail.com>
2013-08-02 18:40:44 +0400

    initial commit to heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Estocada::Application.config.secret_key_base = '74b366639cef318381692c1f192f123c4b46765846f09e0b9b424cb28ad25a357db1e7204052a3b472f82470d93c316e64267935599f9325bf4c677bebac4670'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Estocada::Application.config.session_store :cookie_store, key: '_estocada_session'
